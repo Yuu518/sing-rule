a0cc21053c9a95eb3aa091c1a023608bc862e45e
侑 <50163161+Yuu518@users.noreply.github.com>
2023-12-11 11:46:01 +0800

    Update convert.sh

--- convert/convert.sh
+++ convert/convert.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 mkdir -p rule_set_ip
-./mosdns v2dat unpack-ip -o ./geoip/ geoip.dat
-list=($(ls ./geoip | sed 's/geoip_//g' | sed 's/\.txt//g'))
+./mosdns v2dat unpack-ip -o ./rule_set_ip/ geoip.dat
+list=($(ls ./rule_set_ip | sed 's/geoip_//g' | sed 's/\.txt//g'))
 for ((i = 0; i < ${#list[@]}; i++)); do
-	sed -i 's/^/        "/g' ./geoip/geoip_${list[i]}.txt
-	sed -i 's/$/",/g' ./geoip/geoip_${list[i]}.txt
-	sed -i '1s/^/{\n  "version": 1,\n  "rules": [\n    {\n      "ip_cidr": [\n/g' ./geoip/geoip_${list[i]}.txt
-	sed -i '$ s/,$/\n      ]\n    }\n  ]\n}/g' ./geoip/geoip_${list[i]}.txt
-	mv ./geoip/geoip_${list[i]}.txt ./geoip/${list[i]}.json
-	./sing-box rule-set compile "./geoip/${list[i]}.json" -o ./geoip/${list[i]}.srs
+	sed -i 's/^/        "/g' ./rule_set_ip/geoip_${list[i]}.txt
+	sed -i 's/$/",/g' ./rule_set_ip/geoip_${list[i]}.txt
+	sed -i '1s/^/{\n  "version": 1,\n  "rules": [\n    {\n      "ip_cidr": [\n/g' ./rule_set_ip/geoip_${list[i]}.txt
+	sed -i '$ s/,$/\n      ]\n    }\n  ]\n}/g' ./rule_set_ip/geoip_${list[i]}.txt
+	mv ./rule_set_ip/geoip_${list[i]}.txt ./rule_set_ip/${list[i]}.json
+	./sing-box rule-set compile "./rule_set_ip/${list[i]}.json" -o ./rule_set_ip/${list[i]}.srs
 done
 
 list=($(./sing-box geosite list | sed 's/ (.*)$//g'))
 mkdir -p rule_set_site
 for ((i = 0; i < ${#list[@]}; i++)); do
 	./sing-box geosite export ${list[i]} -o ./geosite/${list[i]}.json
-	./sing-box rule-set compile ./geosite/${list[i]}.json -o ./geosite/${list[i]}.srs
+	./sing-box rule-set compile ./rule_set_site/${list[i]}.json -o ./rule_set_site/${list[i]}.srs
 done
 
 # mkdir -p mixed
