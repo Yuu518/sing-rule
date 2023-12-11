9110c7c5c6c09815eb0985fc967f8c7bafdf8525
侑 <50163161+Yuu518@users.noreply.github.com>
2023-12-11 11:35:00 +0800

    Update convert.sh

--- convert/convert.sh
+++ convert/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p geoip
+mkdir -p rule_set_ip
 ./mosdns v2dat unpack-ip -o ./geoip/ geoip.dat
 list=($(ls ./geoip | sed 's/geoip_//g' | sed 's/\.txt//g'))
 for ((i = 0; i < ${#list[@]}; i++)); do
@@ -13,19 +13,19 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 done
 
 list=($(./sing-box geosite list | sed 's/ (.*)$//g'))
-mkdir -p geosite
+mkdir -p rule_set_site
 for ((i = 0; i < ${#list[@]}; i++)); do
 	./sing-box geosite export ${list[i]} -o ./geosite/${list[i]}.json
 	./sing-box rule-set compile ./geosite/${list[i]}.json -o ./geosite/${list[i]}.srs
 done
 
-mkdir -p mixed
-for file in $(find geoip -type f | grep -v srs | awk -F "/" '{print $NF}'); do
-	if [ -n "$(find geosite -type f -iname "$file")" ]; then
-		file=$(find ./geosite -type f -iname "$file" | awk -F"/" '{print $NF}' | sed 's/\.json//g')
-		head -n -3 ./geoip/${file}.json >./mixed/${file}.json
-		sed -i 's/]/],/g' ./mixed/${file}.json
-		tail -n +5 ./geosite/${file}.json >>./mixed/${file}.json
-		./sing-box rule-set compile ./mixed/${file}.json -o ./mixed/${file}.srs
-	fi
-done
+# mkdir -p mixed
+# for file in $(find geoip -type f | grep -v srs | awk -F "/" '{print $NF}'); do
+# 	if [ -n "$(find geosite -type f -iname "$file")" ]; then
+# 		file=$(find ./geosite -type f -iname "$file" | awk -F"/" '{print $NF}' | sed 's/\.json//g')
+# 		head -n -3 ./geoip/${file}.json >./mixed/${file}.json
+# 		sed -i 's/]/],/g' ./mixed/${file}.json
+# 		tail -n +5 ./geosite/${file}.json >>./mixed/${file}.json
+# 		./sing-box rule-set compile ./mixed/${file}.json -o ./mixed/${file}.srs
+# 	fi
+# done
